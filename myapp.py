4e396cb55e9af8c9d88d598ce42d55122e6e7fac
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-07-31 23:44:16 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -35,16 +35,15 @@ def login():
     col2.markdown("Let us know who's visiting. Are you a/an ...")
     with col2:
         col21, col22, col23 = st.columns([1,1,1])
-        role_as = col21.button("Aspiring Student")
-        role_fl = col22.button("Fellow")
-        role_mt = col23.button("Mentor") 
+        # role_as = col21.button("Aspiring Student")
+        # role_fl = col22.button("Fellow")
+        # role_mt = col23.button("Mentor") 
         st.write(st.session_state.role_chosen)
         
         if st.session_state.vote == None: 
 
-            if role_as:
-                st.session_state.role_chosen == role_as
-                
+            if col21.button("Aspiring Student"):
+                st.session_state.role_chosen == "Aspiring Student"
                 if st.session_state.role_chosen == True:
                     role = role_as
                     st.session_state.vote = {"role": role}
