8f520630748acf192190bfc4d50253fb954db1b7
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-01 20:06:20 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -37,7 +37,7 @@ def google_connection(client):
     users_mentor = pd.DataFrame(sheet_mentor.get_all_records())
     return sheet_fellow, sheet_mentor, users_fellow, users_mentor
 
-google_connection(client)
+sheet_fellow, sheet_mentor, users_fellow, users_mentor = google_connection(client)
 # st.write(pd.DataFrame(sheet_fellow.get_all_records()))
 # st.write(pd.DataFrame(sheet_mentor.get_all_records()))
 
