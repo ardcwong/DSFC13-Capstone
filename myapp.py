8ffd99178dc9b1f6d043b40f79f3d04b4eb405d7
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-02 00:06:43 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -21,9 +21,14 @@ st.set_page_config(
 # LOAD BACKGROUND CSS
 ########################################################
 # Function to load local CSS file
+# Function to load local CSS file
 def load_local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    try:
+        with open(file_name) as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+            st.success("CSS loaded successfully!")  # Debug statement
+    except FileNotFoundError:
+        st.error(f"File {file_name} not found!")
 
 # Load the local CSS file
 load_local_css("data/styles.css")
