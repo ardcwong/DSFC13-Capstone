917d203708a53c53f2f049fdeffbe53f509b6831
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-01 23:57:12 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,13 +4,13 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import openai
 
-# Function to load local CSS file
-def load_local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# # Function to load local CSS file
+# def load_local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# Load the local CSS file
-load_local_css("data/styles.css")
+# # Load the local CSS file
+# load_local_css("data/styles.css")
 
 
 
