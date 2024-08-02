271781e64fd9bcdbe1c15fa5f9c47f036815d2d2
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-02 19:41:11 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -37,31 +37,29 @@ st.set_page_config(
 
 
 
-def set_bg_hack(main_bg):
-    '''
-    A function to unpack an image from root folder and set as bg.
+import base64
 
-    Returns
-    -------
-    The background.
-    '''
-    # set bg name
-    main_bg_ext = main_bg.split('.')[-1]
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
-            background-size: cover;
-            background-attachment: fixed;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
 
-set_bg_hack('data/bg.png')
+set_png_as_page_bg('data/bg.png')
 
 
 ########################################################
