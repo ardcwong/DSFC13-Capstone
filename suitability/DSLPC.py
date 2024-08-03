460ff16cb6d4d120b3a409e249157ade436d02fd
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-04 00:22:26 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -144,7 +144,6 @@ def suitability():
         else:
             if st.session_state.responses and st.session_state.question_index == len(questions):
                 classification = get_classification()
-                st.write(questions_responses)
                 if classification:
                     st.session_state.chat_history.append(("AI", classification))
                     st.session_state.question_index += 1
