215e8098b0bb670d5772d8159db33ea99d7cb0ba
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-04 02:18:11 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -108,10 +108,13 @@ def suitability():
             st.chat_message(role).write(message)
         
         if st.session_state.classification:
-            st.markdown(f"<h6 style='text-align: center;'>Could you please give a thumbs up if you find these recommendations specific and tailored to your needs, or a thumbs down if you do not?✨</h6>", unsafe_allow_html=True)
-            st.caption("*Could you please give a thumbs up if you find these recommendations specific and tailored to your needs, or a thumbs down if you do not?*")
+            st.divider()
+            st.markdown(f"<h7 style='text-align: center;'>Could you please give a thumbs up if you find these recommendations specific and tailored to your needs, or a thumbs down if you do not?</h7>", unsafe_allow_html=True)
+            # st.caption("*Could you please give a thumbs up if you find these recommendations specific and tailored to your needs, or a thumbs down if you do not?*")
+            f1,f2,f3 = st.columns()
             sentiment_mapping = [0,1]
-            feedback = st.feedback("thumbs")    
+            feedback = f2.feedback("thumbs")    
+            st.divider()
             if feedback is not None:
                 st.markdown(type(feedback))
                 feedback_score = sentiment_mapping[feedback]
