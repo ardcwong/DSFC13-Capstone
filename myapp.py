51cde893a76ea081d85156fdd48a323c1de12f12
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-03 11:25:23 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -232,7 +232,7 @@ def home():
     
     # col2.image('data/mihplus.png') #                     !!!!ESKWELABS APP IMAGE!!!
     with col2:
-        if st.session_state.userinfo not in []:
+        if st.session_state.userinfo not in None:
             st.title(f"Hi, {str(st.session_state.userinfo['FirstName'].values[0])}!")
             st.subheader("We added these specific apps for you!")
             home_main_content()
