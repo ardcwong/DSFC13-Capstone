1212b58e3ccc9eac3d5380f70501e29e4933bdaa
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-04 02:09:44 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -108,7 +108,7 @@ def suitability():
             st.chat_message(role).write(message)
         
         if st.session_state.classification:
-            st.chat_message("").info("*Could you please give a thumbs up if you find these recommendations specific and tailored to your needs, or a thumbs down if you do not?*")
+            st.chat_message(" ").caption("*Could you please give a thumbs up if you find these recommendations specific and tailored to your needs, or a thumbs down if you do not?*")
             sentiment_mapping = [0,1]
             feedback = st.feedback("thumbs")    
             if feedback is not None:
