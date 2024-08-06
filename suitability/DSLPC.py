e0265ae7924239a9a57cac6406ee3cfa75754915
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-07 01:09:54 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -78,8 +78,8 @@ if st.session_state.BeginAssessment == True:
     with col_main2:
         with st.expander(":blue-background[How it works]", expanded=st.session_state.BeginAssessment):
             st.markdown("""
-            **1. Answer Questions:** Provide responses to a series of questions about your current experience, learning preferences, time commitment, and budget.<br>
-            **2. Get Classified:** Based on your answers, our classifier bot will evaluate and determine the most appropriate learning pathway for you: 
+            1. Answer Questions: *Provide responses to a series of questions about your current experience, learning preferences, time commitment, and budget.*<br>
+            2. Get Classified: *Based on your answers, our classifier bot will evaluate and determine the most appropriate learning pathway for you:* 
             :gray-background[**Eskwelabs' Bootcamp**], :gray-background[**Self-Learning**], :gray-background[**Master's Degree**]""", unsafe_allow_html=True)
 
 # Google Sheets connection function
