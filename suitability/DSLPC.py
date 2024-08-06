246314cb073f997b6eaf9de4678e3fb54b06f8cd
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-07 01:36:31 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -360,7 +360,7 @@ if st.session_state.BeginAssessment == True:
             with cs2:
                 if st.button("Begin Assessment", type="primary", use_container_width = True):
                     st.session_state.BeginAssessment = False
-                    # st.rerun()
+                    st.rerun()
                     
 
 else: 
