c0ef2a273cf8ccaabf119f1571e88a38fc02d12f
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-07 01:18:00 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -75,13 +75,26 @@ if st.session_state.BeginAssessment == True:
     with col_main1:
         with st.expander(":blue-background[**Our Bot**]", expanded=st.session_state.BeginAssessment):
             st.write("Are you unsure about the best way to pursue your data science journey? Our intelligent classifier bot is here to help! By answering a few simple questions about your background, preferences, and goals, our bot will recommend the most suitable learning pathway for you.")
+    
     with col_main2:
         with st.expander(":blue-background[**How it works**]", expanded=st.session_state.BeginAssessment):
             st.markdown("""
             1. Answer Questions: *Provide responses to a series of questions about your current experience, learning preferences, time commitment, and budget.*<br>
             2. Get Classified: *Based on your answers, our classifier bot will evaluate and determine the most appropriate learning pathway for you:* 
             :gray-background[**Eskwelabs' Bootcamp**], :gray-background[**Self-Learning**], :gray-background[**Master's Degree**]""", unsafe_allow_html=True)
+    
+    with st.container(height=450, border=None):
+        st.markdown(f"<h2 style='text-align: center;'>Start Your Journey</h2>", unsafe_allow_html=True)
+        st.divider()
+        st.markdown("""<h5 style='text-align: center;color: #e76f51;'><b><i>Simply click "Begin Assessment" </b></i><i>
+        and follow the prompts to receive your personalized learning pathway recommendation. Empower your data science career with the right guidance tailored to your needs!.</h5>""", unsafe_allow_html=True)
+        
+        st.divider()
 
+
+
+
+        
 # Google Sheets connection function
 def google_connection(client):
     # Open the Google Sheet
