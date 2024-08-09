53781ee7f3f0b923928b9f4b4bfb6b292946e111
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-10 01:48:57 +0800

    Update InstallationGuide.py

--- InstallationGuidePage/InstallationGuide.py
+++ InstallationGuidePage/InstallationGuide.py
@@ -48,9 +48,9 @@ with colA:
                 
                 st.image('data/apple.png')
             with b:
-                # st.subheader("MAC OS")
-                st.markdown("<h4 style='text-align: left;'>MAC OS</h4>", unsafe_allow_html=True)
-                st.caption("***Install on your MAC OS***")
+                st.subheader("MAC OS")
+                # st.markdown("<h4 style='text-align: left;'>MAC OS</h4>", unsafe_allow_html=True)
+                st.markdown("***Install on your MAC OS***")
                 if st.button("Watch", use_container_width=True, type = "primary", help = "Click to Watch Installation Guide for MAC OS"):
                     st.session_state.choose = choose_mac
                     st.session_state.yt_link = youtube_url_mac
