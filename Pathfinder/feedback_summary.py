cff46a1affa03c881f0289be209c558696979402
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-11 02:36:28 +0800

    Update feedback_summary.py

--- Pathfinder/feedback_summary.py
+++ Pathfinder/feedback_summary.py
@@ -180,7 +180,7 @@ else:
 
             # Display each feedback using st.write
             for feedback in st.session_state.feeback_generated:
-                with container():
+                with st.container():
                     st.write(feedback)
     else:
         st.error("Reference Number not found.")
