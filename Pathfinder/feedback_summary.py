2168ac93161d2405358858d1e7d23d27a19c12a7
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-11 00:30:12 +0800

    Update feedback_summary.py

--- Pathfinder/feedback_summary.py
+++ Pathfinder/feedback_summary.py
@@ -62,9 +62,9 @@ def load_category_structure(spreadsheet):
 
 # Function to load the scores dataset from Google Sheet
 
-def load_scores_dataset(spreadsheet):
-    worksheet = spreadsheet.worksheet("Sheet1")
-    data_score = worksheet.get_all_values()
+def load_scores_dataset(spreadsheet2):
+    worksheet2 = spreadsheet2.worksheet("Sheet1")
+    data_score = worksheet2.get_all_values()
     df_score = pd.DataFrame(data_score[1:], columns=data_score[0])
     st.write(df_score)
     return df_score
