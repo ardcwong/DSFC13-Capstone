c795e89dae0c7bafbaeb2d575c03703e46a043f3
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-11 02:39:39 +0800

    Update feedback_summary.py

--- Pathfinder/feedback_summary.py
+++ Pathfinder/feedback_summary.py
@@ -179,7 +179,7 @@ else:
             # st.write(st.session_state.feeback_generated[0])
 
             # Display each feedback using st.write
-            st.write(category_structure["Main Category"])
+            st.write(category_structure["main_category"])
             for feedback in st.session_state.feeback_generated:
                 with st.container(border=True):
                     st.write(feedback)
