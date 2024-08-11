8274cb867d3f2f57dfe4e0bf578fcde75d233d78
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-11 13:26:05 +0800

    Update feedback_summary.py

--- Pathfinder/feedback_summary.py
+++ Pathfinder/feedback_summary.py
@@ -258,7 +258,7 @@ else:
                             """, unsafe_allow_html=True)
                     # Create the DataFrame
                     df_scores_T = pd.DataFrame(list(scores.items()), columns=["Category", "Performance"]).T
-                    df_scores_T.columns = df_scores_T.iloc[0]
+                    df_scores_T.columns = df_scores_T.iloc[0].reset_index()
                     df_scores_T = df_scores_T[1:]
                     st.write(df_scores_T)
                     # Convert the DataFrame to HTML
