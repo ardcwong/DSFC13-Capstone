c08b73fe053a42026ae079a04ad012a4e1e104a9
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-11 23:01:05 +0800

    Update COGenerator.py

--- Ops/COGenerator.py
+++ Ops/COGenerator.py
@@ -31,7 +31,7 @@ if "spreadsheet_courseoutline_ops" not in st.session_state:
 
 # Load CSV file containing the cohort's main topics and subtopics
 def load_and_generate_course_outline(spreadsheet):
-    worksheet = spreadsheet.worksheet("Sheet1")
+    worksheet = spreadsheet.worksheet("Data Science Fellowship Cohort")
     data = worksheet.get_all_values()
     df = pd.DataFrame(data[1:], columns=data[0])
   
