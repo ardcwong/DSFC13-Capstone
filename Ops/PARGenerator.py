ac3c5169220df879b80b01790af066e0f1af26b2
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-11 15:50:38 +0800

    Update PARGenerator.py

--- Ops/PARGenerator.py
+++ Ops/PARGenerator.py
@@ -192,8 +192,7 @@ def score_table_show():
 
 st.dataframe(scores_dataset)
 is_blank = scores_dataset["PARGenTag"] == "N"
-st.write(scores_dataset[is_blank])
-pf_rn = scores_dataset["Reference Number"].tolist()
+pf_rn = scores_dataset["Reference Number"][is_blank].tolist()
 st.selectbox("Choose a Pathfinder Result Reference Number",pf_rn)
 
 
