c20e5e69a2867f9297fe0694a01bb24b9be8b933
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-13 00:01:26 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -399,8 +399,10 @@ else:
         
 
 if st.toggle("Show Work Flow", label_visibility="collapsed"):
-    with st.expander("Work Flow: How It Works", expanded = True):
-        st.image('data/DSLPC_WorkFlow.png')
+    wf1, wf2 = st.columns([8,2])
+    with wf1:
+        with st.expander("Work Flow: How It Works", expanded = True):
+            st.image('data/DSLPC_WorkFlow.png')
 
 
 
