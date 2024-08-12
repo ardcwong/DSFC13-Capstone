d67137658680116a698849f9f5f0addc793d012a
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-12 21:39:50 +0800

    Update pi_app.py

--- Program_Information/pi_app.py
+++ Program_Information/pi_app.py
@@ -206,28 +206,28 @@ def show_pi_chat_memory():
         content = msg['content']
         st.chat_message(role).write(content)
 
-# # Inject CSS to create a fixed container
-# st.markdown("""
-#     <style>
-#     .fixed-container {
-#         position: fixed;
-#         top: 0;
-#         width: 100%;
-#         background-color: white;
-#         z-index: 1000;
+# Inject CSS to create a fixed container
+st.markdown("""
+    <style>
+    .fixed-container {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        background-color: white;
+        z-index: 1000;
 
-#     }
-#     </style>
+    }
+    </style>
     
-#     <div class="fixed-container">
-#         <h1><br>Eskwelabs Data Science Fellowship Information Bot</h1>
-#     </div>
-# """, unsafe_allow_html=True)
-# st.markdown("""<br><br>""", unsafe_allow_html=True)
+    <div class="fixed-container">
+        <h1><br>Eskwelabs Data Science Fellowship Information Bot</h1>
+    </div>
+""", unsafe_allow_html=True)
+st.markdown("""<br>""", unsafe_allow_html=True)
 
-col111, col222, col333, col444 = st.columns([4,1,4,1])
+col111, col222, col333, col444 = st.columns([4,0.5,5,0.5])
 with col111:
-    st.markdown(f"<h2 style='text-align: center;'>Eskwelabs Data Science Fellowship Information Bot</h2>", unsafe_allow_html=True)
+    # st.markdown(f"<h2 style='text-align: center;'>Eskwelabs Data Science Fellowship Information Bot</h2>", unsafe_allow_html=True)
     st.divider()
     st.markdown("""<h5 style='text-align: center;color: #e76f51;'><b><i>Welcome to the Eskwelabs Data Science Fellowship Information Bot!" </b></i><i>
                 <br><br>
