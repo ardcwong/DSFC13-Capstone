125211d227cb0dcb0f22fcf2329d84a63c7d4f43
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-14 00:37:22 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -211,13 +211,13 @@ def suitability():
               
               if st.session_state.feedback_up == 1:
                   # st.markdown("<h6 style='text-align: center;'>.&emsp;.&emsp;.&emsp;.&emsp;.</h6>", unsafe_allow_html=True)
-                  show_ai_response_lpc("You selected 👍🏻 Thanks for your feedback!",avatar_lpc)
+                  show_ai_response_lpc("<b>ou selected 👍🏻 Thanks for your feedback!</b>",avatar_lpc)
                   # st.markdown("<h6 style='text-align: center;'>You selected 👍🏻 Thanks for your feedback!</h6>", unsafe_allow_html=True)
                   # st.markdown("<h6 style='text-align: center;'>.&emsp;.&emsp;.&emsp;.&emsp;.</h6>", unsafe_allow_html=True)
           
               elif st.session_state.feedback_up == 0:
                   # st.markdown("<h6 style='text-align: center;'>.&emsp;.&emsp;.&emsp;.&emsp;.</h6>", unsafe_allow_html=True)
-                  show_ai_response_lpc("<b>You selected 👎🏻 Thanks for your feedback<b>",avatar_lpc)
+                  show_ai_response_lpc("<b>You selected 👎🏻 Thanks for your feedback</b>",avatar_lpc)
                   # st.markdown("<h6 style='text-align: center;'>You selected 👎🏻 Thanks for your feedback!</h6>", unsafe_allow_html=True)
                   # st.markdown("<h6 style='text-align: center;'>.&emsp;.&emsp;.&emsp;.&emsp;.</h6>", unsafe_allow_html=True)
               else:
