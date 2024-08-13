1cd9868a84f81c5d1002c90e8768928c15d8918e
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-13 21:48:19 +0800

    Update DSFBootcampAssistant.py

--- DSFBAssistant/DSFBootcampAssistant.py
+++ DSFBAssistant/DSFBootcampAssistant.py
@@ -81,9 +81,9 @@ def user_avatar():
 
 avatar_url = user_avatar()
 
-def show_user_question(message_text):
+def show_user_question(message_text,avatar_url):
   # Markdown to replicate the chat message
-  avatar_url = "https://avatars.githubusercontent.com/u/45109972?s=40&v=4"  # Replace this with any avatar URL or a local file path
+  # avatar_url = "https://avatars.githubusercontent.com/u/45109972?s=40&v=4"  # Replace this with any avatar URL or a local file path
   
 
   st.markdown(f"""
@@ -213,7 +213,7 @@ if st.session_state.button_clicked == False:
 
 # Display the response 
 if st.session_state.question is not "":
-    show_user_question(st.session_state.question)
+    show_user_question(st.session_state.question, avatar_url)
     st.session_state.response = generate_conversational_response(st.session_state.question, collection)
     st.chat_message("AI").write(st.session_state.response)
 
