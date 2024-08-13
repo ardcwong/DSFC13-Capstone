2cbc56d812866746dbce889560efdfb58b7b0198
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-14 02:19:36 +0800

    Update pi_app.py

--- Program_Information/pi_app.py
+++ Program_Information/pi_app.py
@@ -337,7 +337,14 @@ with col222:
           st.session_state.button_clicked_pi = True
           st.rerun()
 
-
+  if st.session_state.question_pi is not "":
+    update_chat_memory()
+    # show_user_answer_pi(st.session_state.question_pi, avatar_url_user_pi)
+    st.session_state.response_pi = chatbot_response(st.session_state.question_pi, vector_store, st.session_state.pi_chat_history, st.session_state.pi_chat_memory)
+    # st.chat_message("AI").write(st.session_state.response_pi)
+    # show_ai_response_pi(st.session_state.response_pi,avatar_pi)
+    st.session_state.question_pi = ""
+    st.rerun()
 
 
 
@@ -358,13 +365,7 @@ if user_query:
     st.session_state.button_clicked_pi = True
     st.session_state.question_pi = user_query 
       # Display the response 
-    if st.session_state.question_pi is not "":
-      update_chat_memory()
-      # show_user_answer_pi(st.session_state.question_pi, avatar_url_user_pi)
-      st.session_state.response_pi = chatbot_response(st.session_state.question_pi, vector_store, st.session_state.pi_chat_history, st.session_state.pi_chat_memory)
-      # st.chat_message("AI").write(st.session_state.response_pi)
-      # show_ai_response_pi(st.session_state.response_pi,avatar_pi)
-      st.rerun()
+
       # Update chat memory with the latest messages
     # st.rerun()
 
