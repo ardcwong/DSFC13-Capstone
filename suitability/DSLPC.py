efb363b5fba589960a87ca81323b5432a899f56d
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-14 20:21:38 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -191,7 +191,7 @@ if 'BeginAssessment' not in st.session_state:
     st.session_state.BeginAssessment = True
   
 if 'classification' not in st.session_state:
-    st.session_state.classification = []
+    st.session_state.classification = False
 
     # Initialize or retrieve session state
 if 'responses' not in st.session_state:
@@ -415,7 +415,7 @@ if st.session_state.question_index < len(questions):
                 if classification:
                     st.session_state.chat_history.append(("AI", classification))
                     st.session_state.question_index += 1
-                    st.session_state.classification = classification
+                    st.session_state.classification = True
                     # st.rerun()
         # st.rerun()
 else:
@@ -424,7 +424,7 @@ else:
         if classification:
             st.session_state.chat_history.append(("AI", classification))
             st.session_state.question_index += 1
-            st.session_state.classification = classification
+            st.session_state.classification = True
             st.rerun()
     
 ###############################################      
