fbc98d14b9cc12e66861def7c89b6f58139ca76e
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-16 00:54:06 +0800

    Update PARGenerator.py

--- Ops/PARGenerator.py
+++ Ops/PARGenerator.py
@@ -335,7 +335,7 @@ with tab1:
                         scores[main_category] = score_category
                         
                     # with st.spinner("Generating feedback..."):
-                    with st.progress(0, text="Generating feedback...")
+                    with st.progress(0, text="Generating feedback..."):
                         # if st.session_state.feedback_generated == []:
                         st.session_state.feedback_generated = generate_summarized_feedback(scores)
             
