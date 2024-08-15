5fdc8acbb243ec2040b2544ebcc5b54c394a1171
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-15 11:17:08 +0800

    Update DSLPC.py

--- suitability/DSLPC.py
+++ suitability/DSLPC.py
@@ -496,7 +496,7 @@ else:
 #         1. Answer Questions: *Provide responses to a series of questions about your current experience, learning preferences, time commitment, and budget.*<br>
 #         2. Get Classified: *Based on your answers, our classifier bot will evaluate and determine the most appropriate learning pathway for you:* 
 #         :gray-background[**Eskwelabs' Bootcamp**], :gray-background[**Self-Learning**], :gray-background[**Master's Degree**]""", unsafe_allow_html=True)
-with col_main3:
+with st.sidebar:
     if st.button("Start Over", disabled=st.session_state.BeginAssessment, type="primary", use_container_width = True, help = "Press the this button to start over and answer the questions again. Feel free to make any necessary improvements or corrections to enhance your response."):
         st.session_state.responses = []
         st.session_state.question_index = 0
