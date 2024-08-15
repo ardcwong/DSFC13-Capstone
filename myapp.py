b1367129218a879090200c490dd94a584099a46b
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-16 07:44:44 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -248,7 +248,6 @@ def home_main_content():
       	st.write("HI")
 
       	m21, m22, m23 = st.columns([1,1,1])
-		
         m22.image('data/avatar_ai.png', use_column_width = True)
 		st.markdown("""Data Science Learning Path Classifier""")
         st.markdown("""Start Your Journney! An imtelligent bot that classifies the most appropriate learning path for your Data Science Journey!""")
