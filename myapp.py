fe3fec9695009f6b7526670e72c4e772987d98e0
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-16 07:55:31 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -252,7 +252,7 @@ def home_main_content():
         st.markdown("""Start Your Journney! An imtelligent bot that classifies the most appropriate learning path for your Data Science Journey!""")
         m24, m25, m26 = st.columns([1,1,1])
         with m25:	
-        	if st.button("Launch", use_column_width = True)
+        	if st.button("Launch", use_column_width = True):
                 st.switch_page("suitability/DSLPC.py")
 
 def home():
