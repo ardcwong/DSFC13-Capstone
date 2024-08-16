3628359fdc85e8ca3977505a5a17a446a18fd6e9
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-16 20:25:55 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -257,7 +257,7 @@ def home_main_content():
                     Ready To Navigate Your Data Science Journey?
                 </h1>
                 <p style="font-size: 18px; line-height: 1.5; letter-spacing: 1.5px; color: white;">
-                    <strong>Learn and Be Guided with Confidence!</strong> EskwelApps is here to guide you every step of the way. Whether you're exploring the perfect learning path, seeking detailed program insights, or looking for a personalized assessment, we’ve got everything you need to thrive. <strong>Unlock Tools and Resources!</strong> Once enrolled, dive into our comprehensive course outline, get your questions answered with our bootcamp assistant, and easily set up your environment with our installation guide. Let EskwelApps support you throughout your entire data science journey.
+                    <strong>Learn and Be Guided with Confidence!</strong> EskwelApps is here to guide you every step of the way. Whether you're exploring the perfect learning path, seeking detailed program insights, or looking for a personalized assessment, we’ve got everything you need to thrive. <strong><br>Unlock Tools and Resources!</strong> Once enrolled, dive into our comprehensive course outline, get your questions answered with our bootcamp assistant, and easily set up your environment with our installation guide. Let EskwelApps support you throughout your entire data science journey.
                 </p>
             </div>
             """,
