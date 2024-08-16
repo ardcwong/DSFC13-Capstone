7f8f06cec31dd730f33090481951b21fe586afbc
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-16 17:39:26 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -252,7 +252,7 @@ def home_main_content():
         
     m0, m1, m2, m3, m4 = st.columns([0.3, 1,1,1,0.3])
     with m1:
-        with st.container(border = True, height=200):
+        with st.container(border = True, height=500):
             m11, m12, m13 = st.columns([1,1,1])
             with m12:
                 st.image('data/avatar_ai.png', use_column_width=True)
