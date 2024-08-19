86b3c6cee2f6b8a1256f26f5edd538ef83511820
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-19 22:19:26 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -166,7 +166,7 @@ def vote(spreadsheet):
             st.error("Invalid username or password")       
 
     if st.toggle("[For DEMO] Sample Login Credentials"):
-        st.dataframe(users, use_container_width=True, hide_index=True)
+        st.dataframe(users.head(7), use_container_width=True, hide_index=True)
 # st.write(st.session_state.userinfo)
 ########################################################
 # LOG OUT
