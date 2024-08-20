e335447727e33c5bb7ac9f147aaba6a3574bc277
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-08-20 22:30:37 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -530,52 +530,55 @@ def home():
         with st.container(border =True):
             j1,j2 = st.columns([1,1])
             with j1:
-                st.markdown(
-                    f"""
-                    <style>
-                    .tooltip {{
-                      position: relative;
-                      display: inline-block;
-                      cursor: pointer;
-                    }}
+                st.image('data/John.png', use_column_width=True)
+            with j2:
+                st.markdown("""<br><br><br>Meet John Santos, an aspiring Data Scientist with a background in Electrical Engineering. John’s eager to take the next step in his career, seeking a program that challenges him and prepares him for advanced roles in data science.""")
+                # st.markdown(
+                #     f"""
+                #     <style>
+                #     .tooltip {{
+                #       position: relative;
+                #       display: inline-block;
+                #       cursor: pointer;
+                #     }}
                 
-                    .tooltip .tooltiptext {{
-                      visibility: hidden;
-                      width: 400px;
-                      background-color: #fff;
-                      color: #333;
-                      text-align: left; /* Align text to the left */
-                      border-radius: 5px;
-                      padding: 10px;
-                      position: absolute;
-                      z-index: 1;
-                      left: 80%; /* Position next to the image */
-                      top: 50%;
-                      transform: translateX(20%) translateY(-50%); /* Center tooltip box */
-                      opacity: 0;
-                      transition: opacity 0.3s;
-                      white-space: normal; /* Allow text to wrap */
-                      display: flex;
-                      align-items: flex-end; /* Align content to the bottom */
-                      justify-content: flex-start; /* Align content to the left */
-                      box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
-                      font-size: 20px;
-                    }}
+                #     .tooltip .tooltiptext {{
+                #       visibility: hidden;
+                #       width: 400px;
+                #       background-color: #fff;
+                #       color: #333;
+                #       text-align: left; /* Align text to the left */
+                #       border-radius: 5px;
+                #       padding: 10px;
+                #       position: absolute;
+                #       z-index: 1;
+                #       left: 80%; /* Position next to the image */
+                #       top: 50%;
+                #       transform: translateX(20%) translateY(-50%); /* Center tooltip box */
+                #       opacity: 0;
+                #       transition: opacity 0.3s;
+                #       white-space: normal; /* Allow text to wrap */
+                #       display: flex;
+                #       align-items: flex-end; /* Align content to the bottom */
+                #       justify-content: flex-start; /* Align content to the left */
+                #       box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+                #       font-size: 20px;
+                #     }}
                 
-                    .tooltip:hover .tooltiptext {{
-                      visibility: visible;
-                      opacity: 1;
-                    }}
-                    </style>
-                    <div style='display: flex; align-items: center; justify-content: center; width: 100%;'>
-                        <div class='tooltip' style='flex-shrink: 0; width: 100%;'>
-                            <img src='{john_avatar}' style='width: 100%; height: auto; object-fit: contain;'>
-                            <span class="tooltiptext">Meet John Santos, an aspiring Data Scientist with a background in Electrical Engineering. John’s eager to take the next step in his career, seeking a program that challenges him and prepares him for advanced roles in data science.</span>
-                        </div>
-                    </div>
-                    """,
-                    unsafe_allow_html=True
-                )
+                #     .tooltip:hover .tooltiptext {{
+                #       visibility: visible;
+                #       opacity: 1;
+                #     }}
+                #     </style>
+                #     <div style='display: flex; align-items: center; justify-content: center; width: 100%;'>
+                #         <div class='tooltip' style='flex-shrink: 0; width: 100%;'>
+                #             <img src='{john_avatar}' style='width: 100%; height: auto; object-fit: contain;'>
+                #             <span class="tooltiptext">Meet John Santos, an aspiring Data Scientist with a background in Electrical Engineering. John’s eager to take the next step in his career, seeking a program that challenges him and prepares him for advanced roles in data science.</span>
+                #         </div>
+                #     </div>
+                #     """,
+                #     unsafe_allow_html=True
+                # )
 ########################################################################
 #    MAIN PROGRAM
 ########################################################################
